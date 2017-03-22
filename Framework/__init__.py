3632d801a3a844d924f5c1e18022ee3eedccf360
Theo Styles <thus_styles@msn.com>
2017-03-22 19:09:38 +0000

    windows fix

--- Framework/__init__.py
+++ Framework/__init__.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtGui import QIcon
 from GUI.Helpers import PathHelpers
@@ -7,9 +7,9 @@ from GUI.Helpers import PathHelpers
 from GUI.Windows.MainWindow import MainWindow
 import ctypes
 
-
-myappid = u'theostyles.convneuralnet.1.0'
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+if os.name == 'nt':
+    myappid = u'theostyles.convneuralnet.1.0'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 app = QApplication(sys.argv)
 app.setWindowIcon(QIcon(PathHelpers.getPath("images/logo.png")))
 ex = MainWindow()
