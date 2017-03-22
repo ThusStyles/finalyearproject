a0ca981fe1181c36d67914ea906a3fbce453172b
Theo Styles <thus_styles@msn.com>
2017-03-22 19:07:11 +0000

    windows logo

--- Framework/__init__.py
+++ Framework/__init__.py
@@ -5,7 +5,11 @@ from PyQt5.QtGui import QIcon
 from GUI.Helpers import PathHelpers
 
 from GUI.Windows.MainWindow import MainWindow
+import ctypes
 
+
+myappid = u'theostyles.convneuralnet.1.0'
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 app = QApplication(sys.argv)
 app.setWindowIcon(QIcon(PathHelpers.getPath("images/logo.png")))
 ex = MainWindow()
