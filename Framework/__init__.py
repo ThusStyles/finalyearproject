1afeed7ddbadd5c24c9ce0797a574993195ead5d
Theo Styles <thus_styles@msn.com>
2017-03-22 19:00:07 +0000

    logo

--- Framework/__init__.py
+++ Framework/__init__.py
@@ -1,9 +1,12 @@
 import sys
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from GUI.Helpers import PathHelpers
 
 from GUI.Windows.MainWindow import MainWindow
 
 app = QApplication(sys.argv)
+app.setWindowIcon(QIcon(PathHelpers.getPath("images/logo.png")))
 ex = MainWindow()
 sys.exit(app.exec_())
